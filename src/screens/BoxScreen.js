453be2f5032b421ad776e88ce12be5c65b157ea4
Tika Llyn <83469447+tikallyn@users.noreply.github.com>
2022-01-08 16:44:39 -0600

    finished Box screen

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -4,21 +4,37 @@ import { Text, StyleSheet, View } from 'react-native'
 const BoxScreen = () => {
     return (
         <View style={styles.viewStyle}>
-            <Text style={styles.textStyle}>Box One</Text>
-            <Text style={styles.textStyle}>Box Two</Text>
-            <Text style={styles.textStyle}>Box Three</Text>
+            <View style={styles.view1} />
+            <Text style={styles.view2} />
+            <Text style={styles.view3} />
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    textStyle: {
-        fontSize: 30,
-        borderWidth: 2,
-        borderColor: 'purple',
-        margin: 10
+    view1: {
+        height: 100,
+        width: 100,
+        backgroundColor: 'blue',
+        margin: 5
+    },
+    view2: {
+        height: 100,
+        width: 100,
+        backgroundColor: 'green',
+        margin: 5,
+        alignSelf: 'flex-end',
+    },
+    view3: {
+        height: 100,
+        width: 100,
+        backgroundColor: 'purple',
+        margin: 5
     },
     viewStyle: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        height: 200,
         borderWidth: 3,
         borderColor: 'pink',
     }
